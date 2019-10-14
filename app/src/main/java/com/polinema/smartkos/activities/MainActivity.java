7b8bf244562a65c5c9e1c52f8f0e1c8ab34074a7
Faisal Wafa <faisalwafa10@gmail.com>
2019-10-14 23:49:25 +0700

    add toolbar on main activity

---
+++ app/src/main/java/com/polinema/smartkos/activities/MainActivity.java
@@ -0,0 +1,23 @@
+package com.polinema.smartkos.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.polinema.smartkos.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        TextView toolbarTitle = (TextView) myToolbar.findViewById(R.id.toolbar_title);
+        toolbarTitle.setLetterSpacing((float) 0.1);
+    }
+}
